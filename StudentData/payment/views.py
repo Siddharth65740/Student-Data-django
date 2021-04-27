3bca04603b15e825c5fccce42e85619659a3d9f9
bsnl <Siddharth65740>
2021-04-27 10:09:20 +0530

    Minor changes done(bar chart added,admin pic changed)

--- StudentData/payment/views.py
+++ StudentData/payment/views.py
@@ -1,7 +1,4 @@
-from django.shortcuts import render,redirect
-from django.contrib.auth.models import User
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
+
 from .models import payment_info
 from django.forms import MultiWidget
 from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
@@ -15,20 +12,6 @@ class create_view(LoginRequiredMixin,CreateView):
     model = payment_info
     fields = '__all__'
 
-    '''def form_valid(self, form):
-        self.object = form.save()
-        # do something with self.object
-        # remember the import: from django.http import HttpResponseRedirect
-        self.admission.Pending_fees = self.admission.Pending_fees - self.Amount
-        return HttpResponseRedirect(self.get_success_url())
-    '''
-    '''def form_valid(self, form):
-        response = super(create_view, self).form_valid(form)
-        # do something with self.object
-        self.admission.Pending_fees=self.admission.Pending_fees-self.Amount
-        return response
-    '''
-
 class paymentListView(LoginRequiredMixin,ListView):
     model= payment_info
     fields ="__all__"
@@ -41,6 +24,7 @@ class paymentUpdateView(LoginRequiredMixin,UpdateView):
 class detail_payment(LoginRequiredMixin,DetailView):
     model = payment_info
     template_name = 'payment/invoice_print.html'
+    
 
 
 class delete_payment(LoginRequiredMixin,DeleteView):
